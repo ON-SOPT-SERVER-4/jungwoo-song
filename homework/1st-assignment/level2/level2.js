de49b1d5db30e7a895c5b095ae0e1508d32fb9d3
bluayer <ijacsong98@gmail.com>
2020-10-11 01:34:24 +0900

    feat: 1st week level2 with function

---
+++ homework/1st-assignment/level2/level2.js
@@ -0,0 +1,13 @@
+const level2 = require('./level2.json')
+
+const printMember = () => {
+    const { members } = level2.data
+
+    members.forEach(member => {
+        const { name, address, age, hobby } = member
+        console.log(`이름 : ${name}\n거주지: ${address}\n나이: ${age}\n취미: ${hobby}\n\n`)
+    })
+}
+
+printMember()
+
